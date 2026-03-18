635fa02e34015d136d70aa920cb6c42793488973
Arthur <arthurhenrrique1705@gmail.com>
2026-03-18 15:06:22 -0300

    Adequação do código Main

--- core/src/main/java/io/github/malditos_asteroides/Main.java
+++ core/src/main/java/io/github/malditos_asteroides/Main.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import io.github.malditos_asteroides.entities.Ator;
 import io.github.malditos_asteroides.entities.gerenciadores.GerenciadorDeAsteroides;
 import io.github.malditos_asteroides.entities.gerenciadores.GerenciadorDeColisao;
@@ -14,30 +15,64 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
+    private SpriteBatch spriteBatch;
+    private FitViewport viewport;
+
     private Jogador player;
     private GerenciadorDeAsteroides asteroide;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        player = new Jogador(batch);
-        asteroide = new GerenciadorDeAsteroides(batch, player);
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
+
+        player = new Jogador(spriteBatch);
+        asteroide = new GerenciadorDeAsteroides(spriteBatch, player);
+    }
+
+    @Override
+    public void resize (int width, int height) {
+        viewport.update(width, height, true);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0f, 0f, 0f, 1f);
-        batch.begin();
 
-        asteroide.update();
-        player.update(Gdx.graphics.getDeltaTime());
+        input();
+        draw();
+        logic();
+    }
 
-        batch.end();
+    @Override
+    public void pause () {
+    }
+
+    @Override
+    public void resume () {
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
+        spriteBatch.dispose();
+    }
+
+    private void input() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(0f, 0f, 0f, 1f);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+
+
+        spriteBatch.end();
+    }
+
+    private void logic() {
+        asteroide.update();
+        player.update(Gdx.graphics.getDeltaTime());
     }
 }
