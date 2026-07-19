941ffedf65dc534f4063e49052ab1f72f9472238
Arthur <arthurhenrrique1705@gmail.com>
2026-07-19 20:16:16 -0300

    Alterando PooledEngine para Engine

--- core/src/main/java/io/github/malditos_asteroides/factories/EntityFactory.java
+++ core/src/main/java/io/github/malditos_asteroides/factories/EntityFactory.java
@@ -1,10 +1,9 @@
 package io.github.malditos_asteroides.factories;
 
-import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.math.Vector3;
-
-import javax.swing.text.html.parser.Entity;
+import com.badlogic.ashley.core.Entity;
 
 public interface EntityFactory {
-    public Entity create(PooledEngine engine, Vector3 position);
+    Entity create(Engine engine, Vector3 position);
 }
