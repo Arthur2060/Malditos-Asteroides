9fe9a033d5600a17a035e786055fe7051f3f359d
Arthur <arthurhenrrique1705@gmail.com>
2026-07-25 16:58:16 -0300

    Apagendo vestigios de antigo projeto

--- core/src/main/java/io/github/malditos_asteroides/Main.java
+++ core/src/main/java/io/github/malditos_asteroides/Main.java
@@ -4,41 +4,14 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
-import io.github.malditos_asteroides.entities.Player;
-import io.github.malditos_asteroides.entities.Score;
 import io.github.malditos_asteroides.systens.*;
-import io.github.malditos_asteroides.utils.animation.Animator;
 
 public class Main extends Game {
     public SpriteBatch spriteBatch;
 
-    private CollisionSystem collisionSystem;
-    private Engine engine;
-
     @Override
     public void create() {
         spriteBatch = new SpriteBatch();
-
-        Animator animator = new Animator(spriteBatch);
-        Player player = new Player(Gdx.graphics.getWidth() / 2, 0, spriteBatch);
-        AsteroidSystem asteroidManager = new AsteroidSystem(spriteBatch);
-        BulletSystem bulletSystem = new BulletSystem(player, spriteBatch);
-        collisionSystem = new CollisionSystem(
-            player,
-            asteroidManager,
-            bulletSystem
-        );
-        Score score = new Score(spriteBatch);
-
-        engine = new Engine(
-            player,
-            bulletSystem,
-            asteroidManager,
-            score,
-            animator);
-
-        engine.start();
 
         setScreen(new GameScreen(this));
     }
@@ -51,12 +24,6 @@ public class Main extends Game {
     public void render() {
         Gdx.gl20.glClearColor(0f, 0f, 0f, 1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        float delta = Gdx.graphics.getDeltaTime();
-
-        input(delta);
-        draw();
-        logic(delta);
 
         super.render();
     }
@@ -72,23 +39,5 @@ public class Main extends Game {
     @Override
     public void dispose() {
         spriteBatch.dispose();
-    }
-
-    private void input(float delta) {
-        engine.input(delta);
-    }
-
-    private void draw() {
-        ScreenUtils.clear(0f, 0f, 0f, 1f);
-        spriteBatch.begin();
-
-        engine.draw();
-
-        spriteBatch.end();
-    }
-
-    private void logic(float delta) {
-        engine.logic(delta);
-        collisionSystem.logic();
     }
 }
