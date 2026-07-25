c401817774638abe87a21a5a5cf43edd2088c28d
Arthur <arthurhenrrique1705@gmail.com>
2026-07-25 10:46:09 -0300

    Corrigindo bug em TransformComponent, atributo position estava referênciando atributo de Vector3 ao invés de criar um novo objeto.

--- core/src/main/java/io/github/malditos_asteroides/components/TransformComponent.java
+++ core/src/main/java/io/github/malditos_asteroides/components/TransformComponent.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 public class TransformComponent implements Component {
-    public Vector3 position = Vector3.Zero;
+    public Vector3 position = new Vector3(Vector3.Zero);
     public Vector2 scale = new Vector2(1, 1);
     public float rotation = 0f;
 }
